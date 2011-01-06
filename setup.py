497f2523ed0a3eb8e4dade4d0b6df0ed074c7e02
Gabriel Grant <g@briel.ca>
2011-01-06 05:47:42 -0500

    added missing package to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name='django-python-identifier-field',
     version='0.1.0dev',
     author='Gabriel Grant',
-    packages=['python_identifier_field',],
+    packages=[
+        'python_identifier_field',
+        'python_identifier_field.db.models',
+    ],
     license='LGPL',
     long_description=open('README').read(),
 )
