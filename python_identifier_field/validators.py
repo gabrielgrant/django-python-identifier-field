0f4f33fb698cb436643e7fbb8f2a064bdb6afe1c
Gabriel Grant <g@briel.ca>
2011-01-11 19:03:11 -0500

    fixed bug in identifier name validation preventing single character variables

--- python_identifier_field/validators.py
+++ python_identifier_field/validators.py
@@ -3,7 +3,7 @@ import re
 from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 
-py_identifier_re = re.compile(r'^[a-zA-Z_][\w]+$')
+py_identifier_re = re.compile(r'^[a-zA-Z_][\w]*$')
 validate_py_identifier = RegexValidator(
     py_identifier_re,
     _(u"Enter a valid Python identifier consisting of letters, numbers, "
